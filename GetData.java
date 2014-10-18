3a099a8bd714ef48b07ed032ee457d178ff94757
Peter Mauldin <petermauldin1@gmail.com>
2014-10-18 18:48:54 -0500

    Adding first test

---
+++ GetData.java
@@ -0,0 +1,58 @@
+package com.bloomberglp.blpapi.examples;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import com.bloomberglp.blpapi.Datetime;
+import com.bloomberglp.blpapi.Event;
+import com.bloomberglp.blpapi.Element;
+import com.bloomberglp.blpapi.InvalidRequestException;
+import com.bloomberglp.blpapi.Logging;
+import com.bloomberglp.blpapi.Message;
+import com.bloomberglp.blpapi.MessageIterator;
+import com.bloomberglp.blpapi.Name;
+import com.bloomberglp.blpapi.Request;
+import com.bloomberglp.blpapi.Service;
+import com.bloomberglp.blpapi.Session;
+import com.bloomberglp.blpapi.SessionOptions;
+
+public class GetData {
+	private String IP_ADDRESS;
+	private int PORT;
+	private ArrayList<String> d_securities;
+	private ArrayList<String> field;
+        
+        public GetData() {
+            IP_ADDRESS = "10.8.8.1";
+            PORT = 8194;
+            d_securities.add("MSFT US Equity");
+            field.add("DS002");
+        }
+        
+	public static void main(String args[]) {
+		GetData app = new GetData();
+		app.run();
+	}
+
+	private void run() {
+		SessionOptions sessionOptions = new SessionOptions();
+		session.setServerHost(IP_ADDRESS);
+		session.setServerPort(PORT);
+
+		System.out.println("Connecting to " + IP_ADDRESS + ":" + PORT);
+		Session session = new Session(sessionOptions);
+		if(!session.start()) {
+			System.err.println("Failed to start session.");
+			return;
+		}
+		if(!session.openService("//blp/refdata")) {
+			System.err.println("Failed to open //blp/refdata");
+			return;
+		}
+
+		System.out.println("SUCCESS MOTHERFUCKER!");
+
+		// Session session = new Session ?
+	}
+
+}
